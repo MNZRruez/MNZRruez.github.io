903a4648e17f567cdc3af293d3e5b4ad94e014c8
MNZRruez <mnzr.alee@gmail.com>
2023-11-29 19:57:07 +0530

    fixed nav bar

--- Projects/Portfolio/script.js
+++ Projects/Portfolio/script.js
@@ -39,14 +39,31 @@ const allSections = document.querySelector(".main-content");
 
 
 function PageTransitions(){
-  // btn click on active class
-  for(let i = 0; i < sectBtn.length; i++){
-    sectBtn[i].addEventListener("click", function(){
-      let currentBtn = document.querySelectorAll(".active-btn");
-      currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-      this.className += 'active-btn';
-    })
-  }
+
+  allSections.addEventListener('click', (e)=>{
+    const id = e.target.dataset.id;
+
+    if(id){
+      sectBtn.forEach((btn)=>{
+        btn.classList.remove('active','active-btn')
+      })
+      e.target.classList.add('active','active-btn')
+
+      sections.forEach((section)=>{
+        section.classList.remove('active')
+      })
+
+      const element = document.getElementById(id);
+      element.classList.add('active');
+
+    }
+
+  })
+
+
 }
+
+
+
 
 PageTransitions()
